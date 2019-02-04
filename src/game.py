08721d7494fe2b19fd647fb49ee700201d616f25
Ren Lawrence <renlawrence@gmail.com>
2019-02-03 21:20:17 -0500

    Adds some placeholder key actions

--- src/game.py
+++ src/game.py
@@ -36,7 +36,7 @@ class Game():
         self.mouse_x, self.mouse_y = (0, 0)
 
         # setup debug states
-        self.tooltilesetviewstate = ToolTilesetViewState(self.screen, self.p1) 
+        self.tooltilesetviewstate = ToolTilesetViewState(self.screen, self.p1)
 
         # set into state
         self._set_cur_state(START_STATE(self.screen, self.p1, Map(True)))
@@ -53,44 +53,49 @@ class Game():
 
     def mainloop(self):
         while(1):
-          # do not go faster than the framerate
-          msec = self.clock.tick(self.fps)
-          self.playtime += msec / 1000.0
+            # do not go faster than the framerate
+            msec = self.clock.tick(self.fps)
+            self.playtime += msec / 1000.0
 
-          # reset im key events
-          im.reset_events()
-          self.mouse_x, self.mouse_y = pygame.mouse.get_pos()
+            # reset im key events
+            im.reset_events()
+            self.mouse_x, self.mouse_y = pygame.mouse.get_pos()
 
-          # handle events
-          for event in pygame.event.get():
-            if event.type == EVENT_CUSTOM_SWITCH_STATE:
-                self._set_cur_state(event.loadstate)
-            if event.type == EVENT_CUSTOM_CREATE_STATE:
-                self._evoke_new_state(event.createstate)
-            if event.type == pygame.QUIT:
+            # handle events
+            for event in pygame.event.get():
+              if event.type == EVENT_CUSTOM_SWITCH_STATE:
+                  self._set_cur_state(event.loadstate)
+              if event.type == EVENT_CUSTOM_CREATE_STATE:
+                  self._evoke_new_state(event.createstate)
+              if event.type == pygame.QUIT:
+                  pygame.display.quit()
+                  sys.exit()
+              if event.type == pygame.KEYDOWN:
+                  im.set_key_event(event.type, event.key)
+              if event.type == pygame.JOYBUTTONDOWN:
+                  im.set_joy_button_event(event.type, event.button)
+              if event.type == pygame.MOUSEBUTTONDOWN:
+                  im.set_mouse_event(event.type, event.button)
+
+            # let state handle input
+            im.update()
+            self.cur_state.input(im)
+
+            keystate = pygame.key.get_pressed()
+
+            # emergency exit
+            if keystate[K_q] and (keystate[K_LCTRL] or keystate[K_RCTRL]):
                 pygame.display.quit()
                 sys.exit()
-            if event.type == pygame.KEYDOWN:
-                im.set_key_event(event.type, event.key)
-            if event.type == pygame.JOYBUTTONDOWN:
-                im.set_joy_button_event(event.type, event.button)
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                im.set_mouse_event(event.type, event.button)
 
-          # let state handle input
-          im.update()
-          self.cur_state.input(im)
+            if keystate[K_F1]:
+                self.cur_state = self.tooltilesetviewstate
+                self.cur_state._force_draw()
 
-          keystate = pygame.key.get_pressed()
+            if keystate[K_F1]:
+                self.cur_state = self.dataviewstate
+                self.cur_state._force_draw()
 
-          # emergency exit
-          if keystate[K_q] and (keystate[K_LCTRL] or keystate[K_RCTRL]):
-              pygame.display.quit()
-              sys.exit()
 
-          if keystate[K_F1]:
-              self.cur_state = self.tooltilesetviewstate
-              self.cur_state._force_draw()
-
-          self.cur_state.set_fps(self.clock.get_fps())
-          self.cur_state.run_mainloop()
+            self.cur_state.set_fps(self.clock.get_fps())
+            self.cur_state.run_mainloop()
--- src/states/__init__.py
+++ src/states/__init__.py
@@ -2,3 +2,4 @@ from state import State
 from menustate import MenuState
 from mapstate import MapState
 from tooltilesetviewstate import ToolTilesetViewState
+#from dataviewstate import DataViewState
